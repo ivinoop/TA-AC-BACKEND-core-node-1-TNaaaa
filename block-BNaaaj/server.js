ae0fed39223b4e2773f6b06d0a0d07a6dbd1f139
Vinoo <ivinoop@gmail.com>
2022-02-22 22:31:20 +0530

    assignment 1

---
+++ block-BNaaaj/server.js
@@ -0,0 +1,7 @@
+const url = require('url');
+
+let parsedUrl = url.parse('https://airindia.com/fares/calculate?from=delhi&to=detroit', true);
+
+console.log(parsedUrl.query);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.protocol);
